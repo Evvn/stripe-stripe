c5441f3ab37fac182c5db34b75a40729872eb809
Evan <evvn@outlook.com>
2019-03-04 13:34:49 +1100

    try stripe live, createServer will not work on localhost

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import { injectStripe, CardElement } from 'react-stripe-elements'
 import PaymentRequestForm from './PaymentRequest.js'
+const stripe = require("stripe")("sk_test_PvFxZtS5rhJLoPfqZ6fjT4f6")
 
 class CheckoutForm extends React.Component {
   onSubmit = (ev) => {
     ev.preventDefault()
 
     this.props.stripe.createToken({name: 'Evan Evan'}).then(({token}) => {
+
       console.log('Received Stripe token:', token);
+      // charge 'token' with stripe
+      (async () => {
+        const charge = await stripe.charges.create({
+          amount: 131313,
+          currency: 'aud',
+          description: 'Example chargeExample chargeExample chargeExample charge',
+          source: token,
+        })
+      })()
+
     })
   }
 
@@ -19,12 +31,12 @@ class CheckoutForm extends React.Component {
           <CardElement />
         </label>
 
+        <button>Send it</button>
+
         <label>
           Payment Request (if available)
           <PaymentRequestForm />
         </label>
-
-        <button>Send it</button>
       </form>
     )
   }
